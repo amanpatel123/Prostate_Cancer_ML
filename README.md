eb4fa6f30ed8e3c94dc71e4dad83dd3be16984ee
amanpatel123 <patel1wq@uwindsor.ca>
2021-05-26 09:24:32 -0400

    final submision

--- README.md
+++
@@ -1,3 +0,0 @@
-# Prostate_Cancer_ML
-Identifying meaningful gene biomarkers in prostate cancer dataset to guide the treatment of prostate cancer using Machine Learning Toolkit.
-
--- prostate_cancer.py
+++ prostate_cancer.py
@@ -7,9 +7,12 @@ from collections import Counter
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis 
 from sklearn import svm                                  
-
 from sklearn.decomposition import PCA, KernelPCA
-from sklearn.ensemble import IsolationForest
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
@@ -27,8 +30,16 @@ from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 
 from imblearn.over_sampling import SMOTE
 from imblearn.combine import SMOTEENN
+from imblearn.under_sampling import EditedNearestNeighbours 
 
-MODEL = svm.SVC(kernel='linear')
+
+# MODEL = svm.SVC(kernel='linear')
+MODEL = GaussianNB()
+# MODEL = RandomForestClassifier()
+# MODEL = KNeighborsClassifier(n_neighbors=3)
+# MODEL = svm.SVC(kernel='rbf')
+# MODEL = LinearDiscriminantAnalysis()
+
 def load_genomic_data(filename):
      # load the geneomic data
     genomic_df = pd.read_csv(filename)
@@ -62,7 +73,17 @@ def read_data(gfilename, cfilename):
     
     return X, y
 
+def visualize_class_distribution(y, title):
+    # Visualize the percentage of classes
+    counter = Counter(y)
+    label = counter.keys()
+    size = counter.values()
+    wedges, texts, autotexts = plt.pie(size, autopct='%1.1f%%', shadow=True, startangle=90)
+    plt.legend(wedges, label, title=title, loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+    plt.axis('equal')
+    plt.show()
 
+# Dimentionaliy reduction - Not in use since it was causing low accuracy
 def visualize_data(X, y, title):
     # Visualizing dataset for outliers, using PCA prioir to LDA to prevent overfitting (https://stats.stackexchange.com/q/109810)
     pca = PCA(n_components=10)
@@ -78,7 +99,7 @@ def visualize_data(X, y, title):
     fig = plt.figure(figsize=(6,6))
     plt.scatter(pca_lda_reduced_data[:,0], pca_lda_reduced_data[:,1], c=y, cmap=matplotlib.colors.ListedColormap(colors), alpha=0.7)
     plt.title(title)
-    
+    plt.show()
     
 def prepare_inputs(X_train, X_test):
     scaler = MinMaxScaler()
@@ -90,41 +111,53 @@ def remove_low_variance_feature(X):
     sel = VarianceThreshold()
     return sel.fit_transform(X)
     
-    
 def filter_feature_selection(X_train_norm, y_train, X_test_norm, score_function):
     best_k = SelectKBest(score_func=score_function, k=300)
     fit = best_k.fit(X_train_norm, y_train)
     X_train_fs = fit.transform(X_train_norm)
     X_test_fs = fit.transform(X_test_norm)
     
+    mask = fit.get_support() #list of booleans
+    best_feature_list = [] # The list of your K best features
+    
+    for bool, feature in zip(mask, X_train.columns):
+        if bool:
+            best_feature_list.append(feature)
+            
+    # print(pd.DataFrame(X_train_fs, columns=new_features))
+    
     # DONOT REMOVE 
     # dfscores = pd.DataFrame(fit.scores_)
     # dfcolumns = pd.DataFrame(X_train.columns)
     # featureScores = pd.concat([dfcolumns, dfscores], axis=1)
     # featureScores.columns = ['Features','Score']
-    # best = featureScores.nlargest(40,'Score')
+    # best = featureScores.nlargest(300,'Score')
+    # print(best['Features'].values.sort() == new_features.sort())
     # for f in best['Features']:
     #     featureVoting[f] = featureVoting.get(f, 0) + 1 
-    #best.plot(x='Features', y="Score", kind="bar")
-    return X_train_fs, X_test_fs
+    
+    # best.plot(x='Features', y="Score", kind="bar")
+    return X_train_fs, X_test_fs, best_feature_list
 
-def forward_feature_selecion(X_train_fs, y_train, X_test_fs):
+def forward_feature_selecion(X_train_fs, y_train, X_test_fs, best_feature_list):
     sfs = SFS(MODEL, 
-		k_features=13,
-		forward=True,
-		floating = False,
+		k_features=20,
+		forward=True,                     # when set false this becomes backward feature selection
+		floating = False,             
+        verbose=2,
 		scoring = 'accuracy',
-		cv = 5)
-    fit = sfs.fit(X_train_fs, y_train)
+		cv = 5,
+        n_jobs= -1)
+    fit = sfs.fit(X_train_fs, y_train, custom_feature_names=best_feature_list)
+    print(fit)
     X_train_wfs = fit.transform(X_train_fs)
     X_test_wfs = fit.transform(X_test_fs)
     
     best = sfs.k_feature_names_     # to get the final set of features
-    brint(best)
+    print(best)
 
     return X_train_wfs, X_test_wfs
 
-    
 
 def get_performace_measures(model, X_train, X_test, y_train, y_test, Accuracy_list):
     model = OneVsRestClassifier(model).fit(X_train, y_train)
@@ -139,34 +172,40 @@ def get_performace_measures(model, X_train, X_test, y_train, y_test, Accuracy_li
         print(classifier.score(X_test, is_ith_class))
         Accuracy_list[i] += classifier.score(X_test, is_ith_class)
         
+# Not in use since it was leading to low accuracy
+def outliers_removal(X, y):
+    lof = LocalOutlierFactor()
+    yhat = lof.fit_predict(X)
+    mask = yhat != -1
+    print(mask)
+    return X.iloc[mask, :], y.iloc[mask]
 
 
-# Currenlty not in use
-def dimensionality_reduction(X,y):
-        pca = KernelPCA(kernel='rbf')
-        #pca_reduced_data = pca.fit_transform(X,y)
-        X_transformed = pca.fit_transform(X)
-        return X_transformed
-        #lda = LinearDiscriminantAnalysis()
-        #pca_lda_data = lda.fit_transform(pca_reduced_data,y)
-       
 
+#step 1 read the data
 X, y = read_data('../prad_tcga_genes.csv', '../prad_tcga_clinical_data.csv')
 
-#Resampling dataset, since our data is imbalanced 
-sme = SMOTEENN(random_state=42,smote=SMOTE(random_state=42, k_neighbors=1))
+# Visualize the class distribution before oversampling
+visualize_class_distribution(y, "Before Resampling")
+
+#Resampling the training dataset, since our data is imbalanced (https://datascience.stackexchange.com/a/15633)
+sme = SMOTEENN(random_state=42,smote=SMOTE(random_state=42, k_neighbors=2))
 X, y = sme.fit_resample(X, y)
 print('Resampling of dataset using SMOTEENN %s' % Counter(y), '\n')
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, random_state=42)
+print(X.shape, y.shape)
 
+# Visualize the class distribution after Oversampling
+visualize_class_distribution(y, "After Resampling")
+
+# split the data into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, random_state=42)
 X_train_norm, X_test_norm = prepare_inputs(X_train, X_test)
-X_train_fs, X_test_fs = filter_feature_selection(X_train_norm, y_train, X_test_norm, chi2)
-X_train_wfs, X_test_wfs = forward_feature_selecion(X_train_fs, y_train, X_test_fs)
+X_train_fs, X_test_fs, best_feature_list = filter_feature_selection(X_train_norm, y_train, X_test_norm, mutual_info_classif)
+X_train_wfs, X_test_wfs = forward_feature_selecion(X_train_fs, y_train, X_test_fs, best_feature_list)
 
 Accuracy_list =  [0, 0, 0, 0, 0]
 get_performace_measures(MODEL, X_train_wfs, X_test_wfs, y_train, y_test, Accuracy_list)
-
     
 print("-----------------------------------------------")
 # print("Accuracy: ", round(mean(Accuracy_list), 4))
